f4c6c16d7a2cf01c8493919c7669bc63295c664f
Aleksandr Razumov <ar@cydev.ru>
2017-02-18 22:38:16 +0300

    attributes: add XOR-{PEER,RELAYED}-ADDRESS

---
+++ peeraddr.go
@@ -0,0 +1,29 @@
+package turn
+
+import (
+	"net"
+
+	"github.com/ernado/stun"
+)
+
+// PeerAddress implements XOR-PEER-ADDRESS attribute.
+//
+// The XOR-PEER-ADDRESS specifies the address and port of the peer as
+// seen from the TURN server. (For example, the peer's server-reflexive
+// transport address if the peer is behind a NAT.)
+//
+// https://trac.tools.ietf.org/html/rfc5766#section-14.3
+type PeerAddress struct {
+	IP   net.IP
+	Port int
+}
+
+// AddTo adds XOR-PEER-ADDRESS to message.
+func (a *PeerAddress) AddTo(m *stun.Message) error {
+	return (*stun.XORMappedAddress)(a).AddToAs(m, stun.AttrXORPeerAddress)
+}
+
+// AddTo decodes XOR-PEER-ADDRESS from message.
+func (a *PeerAddress) GetFrom(m *stun.Message) error {
+	return (*stun.XORMappedAddress)(a).GetFromAs(m, stun.AttrXORPeerAddress)
+}
---
+++ relayedaddr.go
@@ -0,0 +1,29 @@
+package turn
+
+import (
+	"net"
+
+	"github.com/ernado/stun"
+)
+
+// RelayedAddress implements XOR-RELAYED-ADDRESS attribute.
+//
+// The XOR-PEER-ADDRESS specifies the address and port of the peer as
+// seen from the TURN server. (For example, the peer's server-reflexive
+// transport address if the peer is behind a NAT.)
+//
+// https://trac.tools.ietf.org/html/rfc5766#section-14.5
+type RelayedAddress struct {
+	IP   net.IP
+	Port int
+}
+
+// AddTo adds XOR-PEER-ADDRESS to message.
+func (a *RelayedAddress) AddTo(m *stun.Message) error {
+	return (*stun.XORMappedAddress)(a).AddToAs(m, stun.AttrXORRelayedAddress)
+}
+
+// AddTo decodes XOR-PEER-ADDRESS from message.
+func (a *RelayedAddress) GetFrom(m *stun.Message) error {
+	return (*stun.XORMappedAddress)(a).GetFromAs(m, stun.AttrXORRelayedAddress)
+}
