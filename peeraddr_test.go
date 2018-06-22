bd4df431b6eeff0676194d9b1b8a86231cd874d7
Aleksandr Razumov <ar@cydev.ru>
2018-06-22 19:58:45 +0300

    attr.{peer,relayed}addr: add test for String()

--- peeraddr_test.go
+++ peeraddr_test.go
@@ -13,6 +13,11 @@ func TestPeerAddress(t *testing.T) {
 		IP:   net.IPv4(111, 11, 1, 2),
 		Port: 333,
 	}
+	t.Run("String", func(t *testing.T) {
+		if a.String() != "111.11.1.2:333" {
+			t.Error("invalid string")
+		}
+	})
 	m := new(stun.Message)
 	if err := a.AddTo(m); err != nil {
 		t.Fatal(err)
