78b8a771e20bf6703768fd645afb76eef107d11c
Aleksandr Razumov <ar@cydev.ru>
2018-08-11 03:28:59 +0300

    e2e: slightly refactor code

--- e2e/main.go
+++ e2e/main.go
@@ -17,12 +17,25 @@ const (
 	peerPort = 56780
 )
 
-func main() {
-	flag.Parse()
+// resolve tries to resolve provided address multiple times, waiting
+// between attempts and calling panic if it fails after 10 attempts.
+func resolve(host string, port int) *net.UDPAddr {
+	addr := fmt.Sprintf("%s:%d", host, port)
 	var (
-		serverAddr *net.UDPAddr
-		echoAddr   *net.UDPAddr
+		resolved   *net.UDPAddr
+		resolveErr error
 	)
+	for i := 0; i < 10; i++ {
+		resolved, resolveErr = net.ResolveUDPAddr(udp, addr)
+		if resolveErr == nil {
+			return resolved
+		}
+		time.Sleep(time.Millisecond * 300 * time.Duration(i))
+	}
+	panic(resolveErr)
+}
+
+func newLogger() *zap.Logger {
 	logCfg := zap.NewDevelopmentConfig()
 	logCfg.DisableCaller = true
 	logCfg.DisableStacktrace = true
@@ -35,66 +48,55 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	return logger
+}
 
+func runPeer(logger *zap.Logger) {
+	laddr, err := net.ResolveUDPAddr(udp, fmt.Sprintf(":%d", peerPort))
+	if err != nil {
+		logger.Fatal("failed to resolve UDP addr", zap.Error(err))
+	}
+	c, err := net.ListenUDP(udp, laddr)
+	if err != nil {
+		logger.Fatal("failed to listen", zap.Error(err))
+	}
+	logger.Info("listening as echo server", zap.Stringer("laddr", c.LocalAddr()))
+	for {
+		// Starting echo server.
+		buf := make([]byte, 1024)
+		n, addr, err := c.ReadFromUDP(buf)
+		if err != nil {
+			logger.Fatal("failed to read", zap.Error(err))
+		}
+		logger.Info("got message",
+			zap.String("body", string(buf[:n])),
+			zap.Stringer("raddr", addr),
+		)
+		// Echoing back.
+		if _, err := c.WriteToUDP(buf[:n], addr); err != nil {
+			logger.Fatal("failed to write back", zap.Error(err))
+		}
+		logger.Info("echoed back",
+			zap.Stringer("raddr", addr),
+		)
+	}
+}
+
+func main() {
+	flag.Parse()
+	logger := newLogger()
 	if flag.Arg(0) == "peer" {
-		laddr, err := net.ResolveUDPAddr(udp, fmt.Sprintf(":%d", peerPort))
-		if err != nil {
-			logger.Fatal("failed to resolve UDP addr", zap.Error(err))
-		}
-		c, err := net.ListenUDP(udp, laddr)
-		if err != nil {
-			logger.Fatal("failed to listen", zap.Error(err))
-		}
-		logger.Info("listening as echo server", zap.Stringer("laddr", c.LocalAddr()))
-		for {
-			// Starting echo server.
-			buf := make([]byte, 1024)
-			n, addr, err := c.ReadFromUDP(buf)
-			if err != nil {
-				logger.Fatal("failed to read", zap.Error(err))
-			}
-			logger.Info("got message",
-				zap.String("body", string(buf[:n])),
-				zap.Stringer("raddr", addr),
-			)
-			// Echoing back.
-			if _, err := c.WriteToUDP(buf[:n], addr); err != nil {
-				logger.Fatal("failed to write back", zap.Error(err))
-			}
-			logger.Info("echoed back",
-				zap.Stringer("raddr", addr),
-			)
-		}
+		runPeer(logger)
 	}
-
 	// Resolving server and peer addresses.
-	for i := 0; i < 10; i++ {
-		serverAddr, err = net.ResolveUDPAddr("udp", fmt.Sprintf("turn-server:%d", turn.DefaultPort))
-		if err == nil {
-			break
-		}
-		time.Sleep(time.Millisecond * 300 * time.Duration(i))
-	}
-	if err != nil {
-		panic(err)
-	}
-	for i := 0; i < 10; i++ {
-		echoAddr, err = net.ResolveUDPAddr("udp", fmt.Sprintf("turn-peer:%d", peerPort))
-		if err == nil {
-			break
-		}
-		time.Sleep(time.Millisecond * 300 * time.Duration(i))
-	}
-	if err != nil {
-		panic(err)
-	}
-
+	var (
+		serverAddr = resolve("turn-server", turn.DefaultPort)
+		echoAddr   = resolve("turn-peer", peerPort)
+	)
 	// Creating connection from client to server.
 	c, err := net.DialUDP(udp, nil, serverAddr)
 	if err != nil {
-		logger.Fatal("failed to dial to TURN server",
-			zap.Error(err),
-		)
+		logger.Fatal("failed to dial to TURN server", zap.Error(err))
 	}
 	logger.Info("dialed server",
 		zap.Stringer("laddr", c.LocalAddr()),
