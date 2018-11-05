6756ead12e186f4831841ad2fa16545962e6d4eb
Aleksandr Razumov <ar@gortc.io>
2018-11-05 10:49:14 +0300

    multiplexer: improve test coverage

--- multiplexer_test.go
+++ multiplexer_test.go
@@ -2,7 +2,9 @@ package turn
 
 import (
 	"io"
+	"net"
 	"testing"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -40,6 +42,54 @@ func TestMultiplexer(t *testing.T) {
 		}))
 		if logs.Len() < 1 {
 			t.Error("no errors logged")
+		}
+	})
+	t.Run("AppData", func(t *testing.T) {
+		core, logs := observer.New(zapcore.ErrorLevel)
+		logger := zap.New(core)
+		connL, connR := net.Pipe()
+		m := newMultiplexer(connR, logger)
+		go func() {
+			if err := connL.SetWriteDeadline(time.Now().Add(time.Second)); err != nil {
+				t.Error(err)
+			}
+			if _, err := connL.Write([]byte{1, 2, 3, 4}); err != nil {
+				t.Error(err)
+			}
+		}()
+		buf := make([]byte, 1024)
+		if _, err := m.dataL.Read(buf); err != nil {
+			t.Error(err)
+		}
+		if logs.Len() > 0 {
+			t.Error("no logs expected")
+		}
+	})
+	t.Run("Write error", func(t *testing.T) {
+		core, logs := observer.New(zapcore.WarnLevel)
+		logger := zap.New(core)
+		connL, connR := net.Pipe()
+		m := newMultiplexer(connR, logger)
+		if err := m.dataR.Close(); err != nil {
+			t.Error(err)
+		}
+		if err := m.dataL.Close(); err != nil {
+			t.Error(err)
+		}
+		if err := connL.SetWriteDeadline(time.Now().Add(time.Second)); err != nil {
+			t.Error(err)
+		}
+		if _, err := connL.Write([]byte{1, 2, 3, 4}); err != nil {
+			t.Error(err)
+		}
+		timeout := time.Tick(time.Second)
+		for logs.Len() < 1 {
+			select {
+			case <-timeout:
+				t.Error("timed out waiting for logs")
+			default:
+				continue
+			}
 		}
 	})
 }
