a4eda4ef5f360e96f1e0327a02c04eba1a799d35
Aleksandr Razumov <ar@gortc.io>
2019-01-28 19:25:10 +0300

    client.permission.test: add test for auth

--- client_permission_test.go
+++ client_permission_test.go
@@ -1,6 +1,7 @@
 package turn
 
 import (
+	"bytes"
 	"net"
 	"testing"
 	"time"
@@ -456,6 +457,133 @@ func TestPermission(t *testing.T) {
 				if err := connL.Close(); err != nil {
 					t.Error(err)
 				}
+				testutil.EnsureNoErrors(t, logs)
+			})
+			t.Run("Authenticated", func(t *testing.T) {
+				core, logs := observer.New(zapcore.DebugLevel)
+				logger := zap.New(core)
+				connL, connR := net.Pipe()
+				stunClient := &testSTUN{}
+				c, createErr := NewClient(ClientOptions{
+					Log:         logger,
+					Conn:        connR, // should not be used
+					STUN:        stunClient,
+					RefreshRate: time.Microsecond,
+
+					Username: "user",
+					Password: "secret",
+				})
+				if createErr != nil {
+					t.Fatal(createErr)
+				}
+				integrity := stun.NewLongTermIntegrity("user", "realm", "secret")
+				serverNonce := stun.NewNonce("nonce")
+				stunClient.indicate = func(m *stun.Message) error {
+					t.Fatal("should not be called")
+					return nil
+				}
+				stunClient.do = func(m *stun.Message, f func(e stun.Event)) error {
+					if m.Type != AllocateRequest {
+						t.Errorf("bad request type: %s", m.Type)
+					}
+					var (
+						nonce    stun.Nonce
+						username stun.Username
+					)
+					if parseErr := m.Parse(&nonce, &username); parseErr != nil {
+						f(stun.Event{
+							Message: stun.MustBuild(m, stun.NewType(stun.MethodAllocate, stun.ClassErrorResponse),
+								stun.NewRealm("realm"),
+								serverNonce,
+								stun.CodeUnauthorized,
+								stun.Fingerprint,
+							),
+						})
+						return nil
+					}
+					if !bytes.Equal(nonce, serverNonce) {
+						t.Error("nonces not equal")
+					}
+					if integrityErr := integrity.Check(m); integrityErr != nil {
+						t.Errorf("integrity check failed: %v", integrityErr)
+					}
+					f(stun.Event{
+						Message: stun.MustBuild(m, stun.NewType(stun.MethodAllocate, stun.ClassSuccessResponse),
+							&RelayedAddress{
+								Port: 1113,
+								IP:   net.IPv4(127, 0, 0, 2),
+							},
+							stun.Fingerprint,
+						),
+					})
+					return nil
+				}
+				a, allocErr := c.Allocate()
+				if allocErr != nil {
+					t.Fatal(allocErr)
+				}
+				peer := &net.UDPAddr{
+					IP:   net.IPv4(127, 0, 0, 1),
+					Port: 1001,
+				}
+				var reqCount int
+				secondReq := make(chan struct{})
+				stunClient.do = func(m *stun.Message, f func(e stun.Event)) error {
+					var (
+						nonce    stun.Nonce
+						username stun.Username
+					)
+					if parseErr := m.Parse(&nonce, &username); parseErr != nil {
+						t.Error("nonce and username expected")
+					}
+					if !bytes.Equal(nonce, serverNonce) {
+						t.Error("nonces not equal")
+					}
+					if integrityErr := integrity.Check(m); integrityErr != nil {
+						t.Errorf("integrity check failed: %v", integrityErr)
+					}
+					switch m.Type {
+					case stun.NewType(stun.MethodCreatePermission, stun.ClassRequest):
+						f(stun.Event{
+							Message: stun.MustBuild(m, stun.NewType(m.Type.Method, stun.ClassSuccessResponse),
+								integrity,
+								stun.Fingerprint,
+							),
+						})
+					case stun.NewType(stun.MethodChannelBind, stun.ClassRequest):
+						reqCount++
+						f(stun.Event{
+							Message: stun.MustBuild(m,
+								stun.NewType(m.Type.Method, stun.ClassSuccessResponse),
+								integrity,
+								stun.Fingerprint,
+							),
+						})
+						if reqCount == 2 {
+							secondReq <- struct{}{}
+						}
+					default:
+						t.Fatalf("unexpected type: %s", m.Type)
+					}
+					return nil
+				}
+				p, permErr := a.CreateUDP(peer)
+				if permErr != nil {
+					t.Fatal(allocErr)
+				}
+				if err := p.Bind(); err != nil {
+					t.Fatal(err)
+				}
+				select {
+				case <-secondReq:
+					// ok
+				case <-time.After(time.Second):
+					t.Error("timed out")
+				}
+				if err := p.Close(); err != nil {
+					t.Error(err)
+				}
+				connL.Close()
 				testutil.EnsureNoErrors(t, logs)
 			})
 		})
